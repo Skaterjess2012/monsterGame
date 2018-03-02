d143024d2c3f39ee5141aa8c043adf3987d43509
Jesse Garcia <skaterjess2012@gmail.com>
2018-03-02 09:13:57 -0700

    added a volume switch to turn on and off music

--- script.js
+++ script.js
@@ -2,17 +2,30 @@
 
 
 // create an array of food items for the monster to eat
-var foodarr = ['pizza', 'icecream', 'trash', 'broccli', 'human', 'half eaten sandwich']; //needs to be a even number; first half, good food, second half bad food
+let foodarr = ['pizza', 'icecream', 'trash', 'broccli', 'human', 'half eaten sandwich']; //needs to be a even number; first half, good food, second half bad food
 let goodfood = foodarr.slice(0, (foodarr.length / 2));
 let badfood = foodarr.slice((foodarr.length / 2), (foodarr.length) + 1);
 let score = 0;
+let songplaying = false;
 let eatSound = new Audio('sounds/eatit.wav');
 let eatItSong = new Audio('sounds/weirldal.wav');
 let trashIt = new Audio('sounds/trashit.wav');
-eatItSong.play();
-console.log(goodfood);
-console.log(badfood);
-var currentfood = "";
+let volUpIcon = document.getElementById('volup');
+let volOffIcon = document.getElementById('voloff');
+let currentfood = "";
+
+try {
+	eatItSong.play();
+	songplaying = true;
+	volUpIcon.style.display = "inline";
+	volOffIcon.style.display = "none";
+}
+catch(err) {
+	console.log("song isnt playing!");
+	songplaying = false;
+	volUpIcon.style.display = "inline";
+	volOffIcon.style.display = "none";
+}
 
 // start the game with a function
 function rungame(){
@@ -22,6 +35,23 @@ function rungame(){
 		score = 0;
 	}
 	document.getElementById('score').innerHTML = score;
+}
+
+function playpause() {
+	switch (songplaying) {
+		case true:
+			eatItSong.pause();
+			songplaying = false;
+			volUpIcon.style.display = "none";
+			volOffIcon.style.display = "inline";
+			break;
+		case false:
+			eatItSong.play();
+			songplaying = true;
+			volOffIcon.style.display = "none";
+			volUpIcon.style.display = "inline";
+			break;
+	}
 }
 
 // randomly select a food to put in the box
